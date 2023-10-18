31c8e7aaa89544b20825abbd6de86937e4390353
Henry Meager <henry.meager@gmail.com>
2023-10-18 14:19:00 +0100

    started incorporating json files into projects section and also added new text weights

---
+++ script.js
@@ -0,0 +1,29 @@
+// // fetch('/json/data.json')
+//     .then((response) => response.json())
+//     .then((items) => {
+//         console.log(items);
+//         let project1 = items[0]
+//     })
+
+fetch('data.json')
+.then(function (response) {
+  return response.json();
+})
+.then(function(data) {
+  console.log(data)
+
+ let p1Heading = document.getElementById('p1heading')
+ p1Heading.innerHTML = data[0].name
+ let p1Image = document.getElementById('p1image')
+ p1Image.src = data[0].img
+ let p1Desc = document.getElementById('p1desc')
+ p1Desc.innerHTML = data[0].desc
+
+ let p2Heading = document.getElementById('p2heading')
+ p2Heading.innerHTML = data[1].name
+ let p2Image = document.getElementById('p2image')
+ p2Image.src = data[1].img
+ let p2Desc = document.getElementById('p2desc')
+ p2Desc.innerHTML = data[1].desc
+});
+
